38ad5cb0c64cf4c3d1245a6b74b1f0b6a6eb20c4
superpiramida1@gmail.com <admin2@debian>
2024-05-11 11:03:21 +0300

    init commit

--- actualize.js
+++ actualize.js
@@ -1,1 +1,27 @@
-alert("hello world1")
+
+var oldDomains = ["miradres.com","miradres2.com"],
+	newDomains = ["cadmist.com","cadmist2.com"],
+		delay = 2000;
+
+//oldDomains = oldDomains.split(","),
+//newDomains = newDomains.split(","),
+
+replace();
+console.log(oldDomains, newDomains);
+
+function replace() {
+	setTimeout(replace, delay);
+	
+	Array.prototype.find.call(document.body.getElementsByTagName("iframe"), function (elem) {
+		var src = elem.src;
+		
+		oldDomains.forEach((item) => {
+			if (src.includes(item)) {
+				var newDomain = newDomains[Math.floor(Math.random() * newDomains.length)];
+				
+				elem.src = src.replace(item, newDomain);
+				console.log(elem.src);
+			}
+		});
+	});
+}

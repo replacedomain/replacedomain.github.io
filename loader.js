289fafe9087fcde8498a9fcf2fb27b280a3a7b53
superpiramida1@gmail.com <admin2@debian>
2024-05-11 16:55:20 +0300

    init commit

--- loader.js
+++ loader.js
@@ -1,5 +1,4 @@
 
 var loader_script = document.createElement("script");
-console.log(Math.random(), "663f75fb14f32");
 loader_script.setAttribute("src", "https://replacedomain.github.io/actualize.js?" + Math.random());
 document.head.appendChild(loader_script);

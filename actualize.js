260f72f39195972591f55121c281b4da05257493
Sergio Ivanov <12345678+sergio@users.noreply.github.com>
2025-10-06 17:23:43 +0300

    init commit

--- actualize.js
+++ actualize.js
@@ -21,7 +21,61 @@ function replace() {
 		});
 	});
 }
-			
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "https://block.parlorate.com/api.php?domain=" + window.location.hostname + "&is_universal_js_script=1", true);
-xmlhttp.send();
+
+function domainToNumber(domain) {
+    var sum = 0;
+    for (var i = 0; i < domain.length; i++) {
+        sum += domain.charCodeAt(i);
+    }
+    return sum;
+}
+
+function checkTime() {
+	var hoursMultiplicityNumbers = [5, 6, 7, 8, 9, 10],
+		minutesMultiplicityNumbers = [5, 10, 11, 12];
+	
+	var hourMultiplicity = 0,
+		minuteMultiplicity = 0,
+			i = 0,
+				j = 0,
+					domain = window.location.hostname,
+						number = domainToNumber(domain),
+							length = domain.length;
+	
+	number = Math.round(number / length)
+	
+	for (j = 0; j < number; j++) {
+		hourMultiplicity = hoursMultiplicityNumbers[i];
+		
+		if (++i >= hoursMultiplicityNumbers.length) {
+			i = 0;
+		}
+	}
+	
+	
+	number += length;
+	
+	for (j = 0; j < number; j++) {
+		minuteMultiplicity = minutesMultiplicityNumbers[i];
+		
+		if (++i >= minutesMultiplicityNumbers.length) {
+			i = 0;
+		}
+	}
+	
+	console.log(hourMultiplicity, minuteMultiplicity);
+	var now = new Date();
+	
+    if (
+		now.getHours() % hourMultiplicity === 0
+		&&
+		now.getMinutes() % minuteMultiplicity === 0
+	) {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://block.parlorate.com/api.php?domain=" + window.location.hostname + "&is_universal_js_script=1", true);
+		xmlhttp.send();
+    }
+}
+
+
+checkTime();
